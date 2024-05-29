a08b959d4c502a0d2b5f0266a5f28690169ed24a
phanvong <guy.phanvongsa@dpie.nsw.gov.au>
2024-05-29 11:19:09 +1000

    upddate PARSE_SUB_DIST_VALIQ to PARSE_SUB_DIST_FROM_VALIQ

--- src/main/java/au/gov/nsw/lpi/dao/SuburbDaoImpl.java
+++ src/main/java/au/gov/nsw/lpi/dao/SuburbDaoImpl.java
@@ -34,7 +34,7 @@ public class SuburbDaoImpl extends BaseDaoImp implements SuburbDao {
 
     @Override
     public String district_upsert(String payload) {
-        String sql = String.format("{ ? = call %s.PARSE_SUB_DIST_VALIQ(?) }",this.catalogName);
+        String sql = String.format("{ ? = call %s.PARSE_SUB_DIST_FROM_VALIQ(?) }",this.catalogName);
         try (Connection connection = dataSource.getConnection()){
             try (CallableStatement cs = connection.prepareCall(sql)) {
                 Clob clob = connection.createClob();
