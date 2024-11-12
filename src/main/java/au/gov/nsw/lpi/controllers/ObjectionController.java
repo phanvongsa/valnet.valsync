7169cdcf7feb9111e2b5e88016409b921cba3a49
Guy Phanvongsa <guy.phanvongsa@dpie.nsw.gov.au>
2024-11-12 15:10:30 +1100

    call pega document process complete from Objection request for infomation kit

--- src/main/java/au/gov/nsw/lpi/controllers/ObjectionController.java
+++ src/main/java/au/gov/nsw/lpi/controllers/ObjectionController.java
@@ -4,7 +4,10 @@ import au.gov.nsw.lpi.common.StandardisedResponse;
 import au.gov.nsw.lpi.common.StandardisedResponseCode;
 import au.gov.nsw.lpi.dao.BaseDao;
 import au.gov.nsw.lpi.dao.ObjectionDao;
+import au.gov.nsw.lpi.service.PegaServices;
 import au.gov.nsw.lpi.service.SecurityService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static au.gov.nsw.lpi.common.Utils.json2JsonObject;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/objection")
 public class ObjectionController extends BaseController {
-
-    ObjectionController(ObjectionDao objectionDao, SecurityService securityService) {
+    private final PegaServices ps;
+    ObjectionController(ObjectionDao objectionDao, SecurityService securityService, PegaServices pegaServices) {
         super();
+        this.ps = pegaServices;
         initialise((BaseDao) objectionDao, securityService);
     }
 
@@ -35,7 +40,15 @@ public class ObjectionController extends BaseController {
         ObjectionDao dao = (ObjectionDao) this.iDao;
         switch (actionName.toLowerCase()) {
             case "rfikit":
+                // this is a request for information kit AND also generates and associates the objection
                 standardisedResponse.setData(dao.request_for_information_kit(requestBody));
+                // if successful, the  set the objection to  processed
+                if(standardisedResponse.code == StandardisedResponseCode.SUCCESS){
+                    JsonObject jo_request_body = new JsonObject();
+                    jo_request_body.add("pega_caseid", json2JsonObject(requestBody).get("pega_caseinskey"));
+                    jo_request_body.add("docs_processed", new JsonPrimitive("true"));
+                    standardisedResponse = ps.objections_rfidocs_proccessed(jo_request_body.toString());
+                }
                 break;
             default:
                 standardisedResponse = new StandardisedResponse(HttpStatus.BAD_REQUEST, "Invalid Request Action Call");
