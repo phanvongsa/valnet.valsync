5ed76de63f6930c55f17927d96faebf2c7ec9d8f
guy <guy.phanvongsa@dpie.nsw.gov.au>
2025-03-07 16:10:32 +1100

    /valsync/nov/preferences/get update proc call for nov_pref_send_json_data()

--- src/main/java/au/gov/nsw/lpi/dao/NovDaoImpl.java
+++ src/main/java/au/gov/nsw/lpi/dao/NovDaoImpl.java
@@ -6,7 +6,7 @@ import javax.sql.DataSource;
 
 @Component
 public class NovDaoImpl extends BaseDaoImp implements NovDao{
-    private final String get_preferences_procedurename = "FUNC_PLACEHOLDER";
+    private final String get_preferences_procedurename = "NOV_PREF_SEND_JSON_DATA";
 
     private final String update_preferences_procedurename = "FUNC_PLACEHOLDER";
 
